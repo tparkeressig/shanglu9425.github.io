4db59cd9772c37c1ed6068b523fd5804d2e074d0
shanglu9425 <shanglu9425ll@hotmail.com>
2017-01-16 00:57:40 -0800

    TEST 111

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var img;
 var canvas
 
 function setup() {
-    canvas = createCanvas(800, 250);
+    reateCanvas(800, 250);
     // Move the canvas so it's inside our <div id="sketch-holder">.
     //canvas.parent('sketch-holder');
     console.log("this is a js comment");
